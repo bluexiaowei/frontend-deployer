8d144fc0ed7e3b44227964aec975517416cfd304
bluexiaowei <bluexiaowei@aliyun.com>
2026-06-05 19:41:14 +0800

    fix: proxy_set_header Host 改为 \$proxy_host，对齐 webpack changeOrigin 行为
    之前透传客户端 Host 头导致后端校验失败返回 403。
    改为 \$proxy_host 后 Host 头变为后端主机地址，与 webpack
    devServer 的 changeOrigin: true 效果一致。

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ server {
     # API 转发到后端
     location ${prefix} {
         proxy_pass ${backend};
-        proxy_set_header Host \$host;
+        proxy_set_header Host \$proxy_host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
